938698f70fce564442a138a4a135ed1d6739387a
Obaid Ur Rahman <Obaidkhan09@users.noreply.github.com>
2021-04-16 10:26:04 +0500

    added useReducer

--- src/RoomChild.js
+++ src/RoomChild.js
@@ -1,13 +1,20 @@
-import React, { useContext } from 'react';
+import React, { useContext, useReducer } from 'react';
 import ValueContext from './ValueContext';
+import ValueReducer from './ValueReducer';
 
 function RoomChild() {
   let Value = useContext(ValueContext);
+  let [state, dispatch ] = useReducer(ValueReducer, 10);
   let IncrVal = Value[1];
   return (
     <div>
-        Value is : {Value} <br/>
-        <button onClick= {()=> {IncrVal(++Value[0])}}>useState Increment</button>
+        <br/> Value is : {Value} <br/>
+        <button onClick= {()=> {IncrVal(++Value[0])}}>useState Increment</button> <br/>
+
+        <br/> Increment & Decrement Using UseReducer: {state} <br/>
+
+        <button onClick= {()=> {dispatch ({type: "INCREMENT"}) } }>useReducer Increment</button> &nbsp; &nbsp;
+        <button onClick= {()=> {dispatch ({type: "DECREMENT"}) } }>useReducer Decrement</button>
     </div>
   );
 }
